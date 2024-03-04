52a7773672208716dd0266d53beadb49df6ed5a6
israabdr <100802852+israabdr@users.noreply.github.com>
2024-03-04 16:41:10 +0100

    Add files via upload

---
+++ testpush.py
@@ -0,0 +1,31 @@
+#Import libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import streamlit as st
+data = pd.read_csv("your_data.csv")
+
+X = data.drop('target_column', axis=1)
+print(X)
+y = data['target_column']
+print(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+clf = RandomForestClassifier()
+clf.fit(X_train, y_train)
+prediction = clf.predict([[18, 9000]])
+print(prediction)
+
+#Streamlit application
+st.title("Machine Learning Classifier")
+
+#Input fields for features
+feature1 = st.number_input("Feature 1", value=0)
+feature2 = st.number_input("Feature 2", value=0)
+#Add more input fields for other features...
+#Validation button
+if st.button("Predict"):
+    # Make predictions
+    prediction = clf.predict([[feature1, feature2]])[0]
+    st.write(f"Predicted class: {prediction}")
